cb38f7d373ad5cf627bc4094c0f4a3ce6e70fa7e
Miri <61881038+miri-fa@users.noreply.github.com>
2022-06-03 18:33:15 +0200

    web app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'presentationProject'`, () => {
+  it(`should have as title 'projectPresentation'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('presentationProject');
+    expect(app.title).toEqual('projectPresentation');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('presentationProject app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('projectPresentation app is running!');
   });
 });
