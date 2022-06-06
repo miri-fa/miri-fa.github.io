ffbda628d27e15445c8a8103bcf7aafc0c4b8e51
Miri <61881038+miri-fa@users.noreply.github.com>
2022-06-06 19:56:59 +0200

    web app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projectPresentation';
+  @Input() cartNumber: number = 0;
+
+  addCart(){
+    this.cartNumber++;
+  }
 }
